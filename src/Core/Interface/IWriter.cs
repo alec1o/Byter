a2f323c7c0d9f106ffa50ea4272cf092a4a29360
ALECIO <alecio@kezero.com>
2022-12-16 16:18:39 +0000

    Create IWriter.cs

---
+++ src/Core/Interface/IWriter.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Byter.Core.Interface
+{
+    internal interface IWriter
+    {
+        int Length { get; }
+        int Position { get; }
+
+        byte[] GetBytes();
+        List<byte> GetList();
+        void Write<T>(T value);
+    }
+}
