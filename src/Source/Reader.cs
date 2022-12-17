3b2644f2d82104ae76594f690d34ca56d07d47e6
ALECIO <alecio@kezero.com>
2022-12-17 11:56:40 +0000

    Create Reader.cs

---
+++ src/Source/Reader.cs
@@ -0,0 +1,365 @@
+using System;
+using System.Text;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Byter.Core.Interface;
+
+namespace Byter
+{
+    public class Reader : IReader, IDisposable
+    {
+        private bool _success;
+        private int _position;
+        private byte[] _buffer;
+
+        public int Length => _buffer.Length;
+        public int Position => _position;
+        public bool Success => _success;
+
+        #region Builder
+
+        // private for IDisposable
+        private Reader() { }
+        
+        public Reader(byte[] buffer, int offset = 0)
+        {           
+            Init(buffer, offset);
+        }
+
+        public Reader(ref Writer writer)
+        {
+            if (writer == null || writer.Length <= 0)
+            {
+                Init(null, 0);
+            }
+            else
+            {
+                Init(writer.GetBytes(), 0);
+            }
+        }
+
+        public Reader(Writer writer)
+        {           
+            if (writer == null || writer.Length <= 0)
+            {
+                Init(null, 0);
+            }
+            else
+            {
+                Init(writer.GetBytes(), 0);
+            }
+        }
+
+        private void Init(byte[] buffer, int offset)
+        {
+            if (buffer == null || buffer.Length <= 0 || (buffer.Length - offset) <= 0)
+            {
+                _buffer = new byte[0];
+                return;
+            }
+
+            _buffer = new byte[buffer.Length - offset];
+            Buffer.BlockCopy(buffer, offset,  _buffer, 0, _buffer.Length);
+        }
+
+        #endregion
+
+        #region Other
+        
+        public void Seek(int position)
+        {
+            if (position <= 0) _position = 0;
+            else _position = position;
+        }
+
+        public T Read<T>()
+        {
+            char prefix = Writer.GetPrefix(typeof(T));
+
+            // byte
+            if (typeof(T) == typeof(byte))
+            {               
+                // compare (buffer prefix) to (type prefix)
+                if (!ValidPrefix(prefix)) return default;  
+
+                // get encoded value
+                var value = _buffer[_position];
+
+                // skip bytes read
+                _position += sizeof(byte);
+
+                return (T)(object) value;
+            }
+
+            // byte[]
+            if (typeof(T) == typeof(byte))
+            {               
+                // compare (buffer prefix) to (type prefix)
+                if (!ValidPrefix(prefix)) return default;
+
+                // get length of bytes
+                int length = BitConverter.ToInt32(_buffer, _position);
+                _position += sizeof(int);
+                if (length <= 0 || length > (Length - _position))
+                {
+                    _success = false;
+                    return default;
+                }
+
+                // get encoded value
+                byte[] value = new byte[length];
+                Buffer.BlockCopy(_buffer, _position, value, 0, value.Length);
+
+                // skip bytes read
+                _position += value.Length;
+
+                return (T)(object) value;
+            }
+
+            // short
+            if(typeof(T) == typeof(short))
+            {
+                // compare (buffer prefix) to (type prefix)
+                if (!ValidPrefix(prefix)) return default;  
+
+                // get encoded value
+                var value = BitConverter.ToInt16(_buffer, _position);
+
+                // skip bytes read
+                _position += sizeof(short);
+
+                return (T)(object) value;
+            }
+
+            // ushort
+            if(typeof(T) == typeof(ushort))
+            {
+                // compare (buffer prefix) to (type prefix)
+                if (!ValidPrefix(prefix)) return default;  
+
+                // get encoded value
+                var value = BitConverter.ToUInt16(_buffer, _position);
+
+                // skip bytes read
+                _position += sizeof(ushort);
+
+                return (T)(object) value;
+            }
+            
+            // int
+            if(typeof(T) == typeof(int))
+            {
+                // compare (buffer prefix) to (type prefix)
+                if (!ValidPrefix(prefix)) return default;  
+
+                // get encoded value
+                var value = BitConverter.ToInt32(_buffer, _position);
+
+                // skip bytes read
+                _position += sizeof(int);
+
+                return (T)(object) value;
+            }
+
+            // uint
+            if(typeof(T) == typeof(uint))
+            {
+                // compare (buffer prefix) to (type prefix)
+                if (!ValidPrefix(prefix)) return default;  
+
+                // get encoded value
+                var value = BitConverter.ToUInt32(_buffer, _position);
+
+                // skip bytes read
+                _position += sizeof(uint);
+
+                return (T)(object) value;
+            }
+
+            // long
+            if(typeof(T) == typeof(long))
+            {
+                // compare (buffer prefix) to (type prefix)
+                if (!ValidPrefix(prefix)) return default;  
+
+                // get encoded value
+                var value = BitConverter.ToInt64(_buffer, _position);
+
+                // skip bytes read
+                _position += sizeof(long);
+
+                return (T)(object) value;
+            }
+
+            // ulong
+            if(typeof(T) == typeof(ulong))
+            {
+                // compare (buffer prefix) to (type prefix)
+                if (!ValidPrefix(prefix)) return default;  
+
+                // get encoded value
+                var value = BitConverter.ToUInt64(_buffer, _position);
+
+                // skip bytes read
+                _position += sizeof(ulong);
+
+                return (T)(object) value;
+            }
+
+            // float
+            if(typeof(T) == typeof(float))
+            {
+                // compare (buffer prefix) to (type prefix)
+                if (!ValidPrefix(prefix)) return default;  
+
+                // get encoded value
+                var value = BitConverter.ToSingle(_buffer, _position);
+
+                // skip bytes read
+                _position += sizeof(float);
+
+                return (T)(object) value;
+            }
+
+            // double
+            if(typeof(T) == typeof(double))
+            {
+                // compare (buffer prefix) to (type prefix)
+                if (!ValidPrefix(prefix)) return default;  
+
+                // get encoded value
+                var value = BitConverter.ToDouble(_buffer, _position);
+
+                // skip bytes read
+                _position += sizeof(double);
+
+                return (T)(object) value;
+            }
+
+            // char
+            if (typeof(T) == typeof(char))
+            {               
+                // compare (buffer prefix) to (type prefix)
+                if (!ValidPrefix(prefix)) return default;  
+
+                // get encoded value
+                var value = BitConverter.ToChar(_buffer, _position);
+
+                // skip bytes read
+                _position += sizeof(char);
+
+                return (T)(object) value;
+            }
+
+            // string
+            if (typeof(T) == typeof(string))
+            {
+                return Read<T>(Encoding.UTF8);
+            }
+
+            _success = false;
+            return default;
+        }
+
+        public T Read<T>(Encoding encode)
+        {
+            char prefix = Writer.GetPrefix(typeof(T));
+
+            // string
+            if (typeof(T) == typeof(byte))
+            {               
+                // compare (buffer prefix) to (type prefix)
+                if (!ValidPrefix(prefix)) return default;
+
+                // get length of bytes
+                int length = BitConverter.ToInt32(_buffer, _position);
+                _position += sizeof(int);
+                if (length <= 0 || length > (Length - _position))
+                {
+                    _success = false;
+                    return default;
+                }
+
+                // get encoded value
+                byte[] bytes = new byte[length];
+                Buffer.BlockCopy(_buffer, _position, bytes, 0, bytes.Length);               
+                
+                // skip bytes read
+                _position += bytes.Length;
+
+                // convert bytes to string
+                string value = encode.GetString(bytes);
+
+                return (T)(object) value;
+            }
+
+            _success = false;
+            return default;
+        }
+
+        #endregion
+
+        private bool ValidPrefix(char prefix)
+        {
+            // compare (buffer prefix) to (type prefix)
+            char signal = BitConverter.ToChar(_buffer, _position);
+
+            if (prefix == signal)
+            {
+                // skip bytes read
+                _position += sizeof(char);
+
+                return true;
+            }
+
+            _success = false;
+            return false;
+        }
+
+
+        #region Dispose
+
+        // Track whether Dispose has been called.
+        private bool disposed = false;
+
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            // Check to see if Dispose has already been called.
+            if(!this.disposed)
+            {
+                // If disposing equals true, dispose all managed and unmanaged resources.
+                if(disposing)
+                {
+                    // Dispose managed resources.
+
+                    // <instance>.Dispose();
+                    _buffer = null;
+                }
+
+                // Call the appropriate methods to clean up unmanaged resources here.
+                // If disposing is false, only the following code is executed.
+
+                // <instance>.Dispose();
+
+                // Note disposing has been done.
+                disposed = true;
+            }
+        }        
+
+        ~Reader()
+        {
+            Dispose(disposing: false);
+        }        
+        
+        #endregion
+
+    }
+}
