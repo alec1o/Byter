17eee79710dd26c4d7faddc2f5cdb022d18f06fe
ALECIO <alecio@kezero.com>
2022-12-17 14:07:56 +0000

    Fix. Fix. Error created for testing

--- src/Source/Reader.cs
+++ src/Source/Reader.cs
@@ -75,194 +75,195 @@ namespace Byter
 
         public T Read<T>()
         {
-            char prefix = Writer.GetPrefix(typeof(T));
-
-            // byte
-            if (typeof(T) == typeof(byte))
-            {
-                // compare (buffer prefix) to (type prefix)
-                if (!ValidPrefix(prefix)) return default;  
-
-                // get encoded value
-                var value = _buffer[_position];
-
-                // skip bytes read
-                _position += sizeof(byte);
-
-                return (T)(object) value;
-            }
-
-            // byte[]
-            else if (typeof(T) == typeof(byte[]))
-            {               
-                // compare (buffer prefix) to (type prefix)
-                if (!ValidPrefix(prefix)) return default;
-
-                // get length of bytes
-                int length = BitConverter.ToInt32(_buffer, _position);
-                _position += sizeof(int);
-                if (length <= 0 || length > (Length - _position))
+            try
                 {
-                    _success = false;
-                    return default;
+                char prefix = Writer.GetPrefix(typeof(T));
+
+                // byte
+                if (typeof(T) == typeof(byte))
+                {
+                    // compare (buffer prefix) to (type prefix)
+                    if (!ValidPrefix(prefix)) return default;  
+
+                    // get encoded value
+                    var value = _buffer[_position];
+
+                    // skip bytes read
+                    _position += sizeof(byte);
+
+                    return (T)(object) value;
                 }
 
-                // get encoded value
-                byte[] value = new byte[length];
-                Buffer.BlockCopy(_buffer, _position, value, 0, value.Length);
+                // byte[]
+                else if (typeof(T) == typeof(byte[]))
+                {               
+                    // compare (buffer prefix) to (type prefix)
+                    if (!ValidPrefix(prefix)) return default;
 
-                // skip bytes read
-                _position += value.Length;
+                    // get length of bytes
+                    int length = BitConverter.ToInt32(_buffer, _position);
+                    _position += sizeof(int);
+                    if (length <= 0 || length > (Length - _position))
+                    {
+                        _success = false;
+                        return default;
+                    }
 
-                return (T)(object) value;
+                    // get encoded value
+                    byte[] value = new byte[length];
+                    Buffer.BlockCopy(_buffer, _position, value, 0, value.Length);
+
+                    // skip bytes read
+                    _position += value.Length;
+
+                    return (T)(object) value;
+                }
+
+                // short
+                else if (typeof(T) == typeof(short))
+                {
+                    // compare (buffer prefix) to (type prefix)
+                    if (!ValidPrefix(prefix)) return default;  
+
+                    // get encoded value
+                    var value = BitConverter.ToInt16(_buffer, _position);
+
+                    // skip bytes read
+                    _position += sizeof(short);
+
+                    return (T)(object) value;
+                }
+
+                // ushort
+                else if (typeof(T) == typeof(ushort))
+                {
+                    // compare (buffer prefix) to (type prefix)
+                    if (!ValidPrefix(prefix)) return default;  
+
+                    // get encoded value
+                    var value = BitConverter.ToUInt16(_buffer, _position);
+
+                    // skip bytes read
+                    _position += sizeof(ushort);
+
+                    return (T)(object) value;
+                }
+                
+                // int
+                else if(typeof(T) == typeof(int))
+                {
+                    // compare (buffer prefix) to (type prefix)
+                    if (!ValidPrefix(prefix)) return default;  
+
+                    // get encoded value
+                    var value = BitConverter.ToInt32(_buffer, _position);
+
+                    // skip bytes read
+                    _position += sizeof(int);
+
+                    return (T)(object) value;
+                }
+
+                // uint
+                else if(typeof(T) == typeof(uint))
+                {
+                    // compare (buffer prefix) to (type prefix)
+                    if (!ValidPrefix(prefix)) return default;  
+
+                    // get encoded value
+                    var value = BitConverter.ToUInt32(_buffer, _position);
+
+                    // skip bytes read
+                    _position += sizeof(uint);
+
+                    return (T)(object) value;
+                }
+
+                // long
+                else if(typeof(T) == typeof(long))
+                {
+                    // compare (buffer prefix) to (type prefix)
+                    if (!ValidPrefix(prefix)) return default;  
+
+                    // get encoded value
+                    var value = BitConverter.ToInt64(_buffer, _position);
+
+                    // skip bytes read
+                    _position += sizeof(long);
+
+                    return (T)(object) value;
+                }
+
+                // ulong
+                else if(typeof(T) == typeof(ulong))
+                {
+                    // compare (buffer prefix) to (type prefix)
+                    if (!ValidPrefix(prefix)) return default;  
+
+                    // get encoded value
+                    var value = BitConverter.ToUInt64(_buffer, _position);
+
+                    // skip bytes read
+                    _position += sizeof(ulong);
+
+                    return (T)(object) value;
+                }
+
+                // float
+                else if(typeof(T) == typeof(float))
+                {
+                    // compare (buffer prefix) to (type prefix)
+                    if (!ValidPrefix(prefix)) return default;  
+
+                    // get encoded value
+                    var value = BitConverter.ToSingle(_buffer, _position);
+
+                    // skip bytes read
+                    _position += sizeof(float);
+
+                    return (T)(object) value;
+                }
+
+                // double
+                else if(typeof(T) == typeof(double))
+                {
+                    // compare (buffer prefix) to (type prefix)
+                    if (!ValidPrefix(prefix)) return default;  
+
+                    // get encoded value
+                    var value = BitConverter.ToDouble(_buffer, _position);
+
+                    // skip bytes read
+                    _position += sizeof(double);
+
+                    return (T)(object) value;
+                }
+
+                // char
+                else if (typeof(T) == typeof(char))
+                {               
+                    // compare (buffer prefix) to (type prefix)
+                    if (!ValidPrefix(prefix)) return default;  
+
+                    // get encoded value
+                    var value = BitConverter.ToChar(_buffer, _position);
+
+                    // skip bytes read
+                    _position += sizeof(char);
+
+                    return (T)(object) value;
+                }
+
+                // string
+                else if (typeof(T) == typeof(string))
+                {
+                    return Read<T>(Encoding.UTF8);
+                }
             }
+            catch { }
 
-            // short
-            else if (typeof(T) == typeof(short))
-            {
-                // compare (buffer prefix) to (type prefix)
-                if (!ValidPrefix(prefix)) return default;  
-
-                // get encoded value
-                var value = BitConverter.ToInt16(_buffer, _position);
-
-                // skip bytes read
-                _position += sizeof(short);
-
-                return (T)(object) value;
-            }
-
-            // ushort
-            else if (typeof(T) == typeof(ushort))
-            {
-                // compare (buffer prefix) to (type prefix)
-                if (!ValidPrefix(prefix)) return default;  
-
-                // get encoded value
-                var value = BitConverter.ToUInt16(_buffer, _position);
-
-                // skip bytes read
-                _position += sizeof(ushort);
-
-                return (T)(object) value;
-            }
-            
-            // int
-            else if(typeof(T) == typeof(int))
-            {
-                // compare (buffer prefix) to (type prefix)
-                if (!ValidPrefix(prefix)) return default;  
-
-                // get encoded value
-                var value = BitConverter.ToInt32(_buffer, _position);
-
-                // skip bytes read
-                _position += sizeof(int);
-
-                return (T)(object) value;
-            }
-
-            // uint
-            else if(typeof(T) == typeof(uint))
-            {
-                // compare (buffer prefix) to (type prefix)
-                if (!ValidPrefix(prefix)) return default;  
-
-                // get encoded value
-                var value = BitConverter.ToUInt32(_buffer, _position);
-
-                // skip bytes read
-                _position += sizeof(uint);
-
-                return (T)(object) value;
-            }
-
-            // long
-            else if(typeof(T) == typeof(long))
-            {
-                // compare (buffer prefix) to (type prefix)
-                if (!ValidPrefix(prefix)) return default;  
-
-                // get encoded value
-                var value = BitConverter.ToInt64(_buffer, _position);
-
-                // skip bytes read
-                _position += sizeof(long);
-
-                return (T)(object) value;
-            }
-
-            // ulong
-            else if(typeof(T) == typeof(ulong))
-            {
-                // compare (buffer prefix) to (type prefix)
-                if (!ValidPrefix(prefix)) return default;  
-
-                // get encoded value
-                var value = BitConverter.ToUInt64(_buffer, _position);
-
-                // skip bytes read
-                _position += sizeof(ulong);
-
-                return (T)(object) value;
-            }
-
-            // float
-            else if(typeof(T) == typeof(float))
-            {
-                // compare (buffer prefix) to (type prefix)
-                if (!ValidPrefix(prefix)) return default;  
-
-                // get encoded value
-                var value = BitConverter.ToSingle(_buffer, _position);
-
-                // skip bytes read
-                _position += sizeof(float);
-
-                return (T)(object) value;
-            }
-
-            // double
-            else if(typeof(T) == typeof(double))
-            {
-                // compare (buffer prefix) to (type prefix)
-                if (!ValidPrefix(prefix)) return default;  
-
-                // get encoded value
-                var value = BitConverter.ToDouble(_buffer, _position);
-
-                // skip bytes read
-                _position += sizeof(double);
-
-                return (T)(object) value;
-            }
-
-            // char
-            else if (typeof(T) == typeof(char))
-            {               
-                // compare (buffer prefix) to (type prefix)
-                if (!ValidPrefix(prefix)) return default;  
-
-                // get encoded value
-                var value = BitConverter.ToChar(_buffer, _position);
-
-                // skip bytes read
-                _position += sizeof(char);
-
-                return (T)(object) value;
-            }
-
-            // string
-            else if (typeof(T) == typeof(string))
-            {
-                return Read<T>(Encoding.UTF8);
-            }
-
-            else
-            {
-                _success = false;
-                return default;
-            }
+            _success = false;
+            return default;
         }
 
         public T Read<T>(Encoding encode)
