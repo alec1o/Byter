7a20813a5cb28e3c5872d202bbeb7cffc5087833
Alecio Furanze <alecio@kezero.com>
2024-04-29 06:54:08 +0200

    impl. getstring in byteextension

--- src/extension/Byte.cs
+++ src/extension/Byte.cs
@@ -1,7 +1,14 @@
+using System.Text;
+
 namespace Byter
 {
     public static class ByteExtension
     {
+        public static string GetString(this byte[] content)
+        {
+            if (content == null || content.Length <= 0) return string.Empty;
+            return StringExtension.Default.GetString(content);
+        }
 
     }
 }
