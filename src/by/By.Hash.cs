91ff17fb8374a087a62148d06bc291bae9bc76a7
Alecio Furanze <alecio@kezero.com>
2024-05-04 03:54:27 +0200

    Create By.Hash.cs

---
+++ src/by/By.Hash.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Byter
+{
+    public partial class By : IBy
+    {
+        public static ushort Hash<T>(T value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
