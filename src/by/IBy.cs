9f94855093096f92f2665f57102b5cbc52d22965
Alecio Furanze <alecio@kezero.com>
2024-05-04 03:05:34 +0200

    Create IBy.cs

---
+++ src/by/IBy.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace Byter
+{
+    internal interface IBy
+    {
+        int Index { get; }
+        bool IsValid { get; }
+        byte[] Buffer { get; }
+        
+        void Reset();
+        void Add<T>(T value);
+        T Get<T>();
+    }
+}
