57c0ab21599af04656ea8f40df523fb653b2965e
Alecio Furanze <alecio@kezero.com>
2024-05-04 03:41:19 +0200

    add available primitive types

--- src/by/By.cs
+++ src/by/By.cs
@@ -1,14 +1,50 @@
 using System;
 using System.Collections.Generic;
+using System.Numerics;
 
 namespace Byter
 {
     public class By : IBy
     {
+        public static readonly Type[] Types;
         public int Index { get; private set; }
         public bool IsValid { get; private set; }
         public byte[] Buffer => _vault.ToArray();
         private readonly List<byte> _vault;
+
+        static By()
+        {
+            Types = new[]
+            {
+                // 1 byte
+                typeof(sbyte),
+                typeof(byte),
+                typeof(bool),
+                
+                // 2 bytes
+                typeof(char),
+                typeof(short),
+                typeof(ushort),
+                
+                // 4 bytes
+                typeof(uint),
+                typeof(int),
+                typeof(float),
+                
+                // 8 bytes
+                typeof(long),
+                typeof(ulong),
+                typeof(double),
+                
+                // 16 bytes
+                typeof(decimal),
+                
+                // dynamic
+                typeof(byte[]),
+                typeof(string),
+                typeof(BigInteger)
+            };
+        }
 
         private By(int index, bool isValid, byte[] buffer)
         {
