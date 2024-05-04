c8ffca733f764698a03d3729254fc686c255e91a
Alecio Furanze <alecio@kezero.com>
2024-05-04 05:06:48 +0200

    impl by.hash

--- src/by/By.Hash.cs
+++ src/by/By.Hash.cs
@@ -1,12 +1,38 @@
 using System;
+using System.Numerics;
 
 namespace Byter
 {
     public partial class By : IBy
     {
-        public static ushort Hash<T>(T value)
+        public static Types Hash<T>(T value)
         {
-            throw new NotImplementedException();
+            if (value == null) return Types.Null;
+            
+            Type type = value.GetType();
+            
+            if (type == typeof(sbyte)) return Types.Sbyte;
+            if (type == typeof(byte)) return Types.Byte;
+            if (type == typeof(bool)) return Types.Bool;
+            if (type == typeof(char)) return Types.Char;
+            if (type == typeof(short)) return Types.Short;
+            if (type == typeof(ushort)) return Types.Ushort;
+            if (type == typeof(uint)) return Types.Uint;
+            if (type == typeof(int)) return Types.Int;
+            if (type == typeof(float)) return Types.Float;
+            if (type.IsEnum) return Types.Enum;
+            if (type == typeof(long)) return Types.Long;
+            if (type == typeof(ulong)) return Types.Ulong;
+            if (type == typeof(double)) return Types.Double;
+            if (type == typeof(decimal)) return Types.Decimal;
+            if (type == typeof(byte[])) return Types.Bytes;
+            if (type == typeof(string)) return Types.String;
+            if (type == typeof(BigInteger)) return Types.BigInteger;
+            if (type.IsClass) return Types.Class;
+            if (type.IsConstructedGenericType) return Types.Struct;
+            if (type.IsArray) return Types.Array;
+            
+            throw new NotImplementedException($"[{nameof(By)}.{nameof(Hash)}] Error: {type} Isn't implemented");
         }
     }
 }
