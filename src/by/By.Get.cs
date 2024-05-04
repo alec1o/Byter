42e98e1c6541773b438b09e17946113b385b3998
Alecio Furanze <alecio@kezero.com>
2024-05-04 07:06:34 +0200

    by.get impl int

--- src/by/By.Get.cs
+++ src/by/By.Get.cs
@@ -6,7 +6,35 @@ namespace Byter
     {
         public T Get<T>()
         {
-            throw new NotImplementedException();
+            if (Buffer.Length <= 0 || Index >= Buffer.Length)
+            {
+                IsValid = false;
+                return default;
+            }
+
+            Types type = Hash(typeof(T));
+
+            switch (type)
+            {
+                case Types.Int:
+                {
+                    if (Index + sizeof(int) + sizeof(sbyte) > _vault.Count) return default;
+                    sbyte prefix = (sbyte)_vault[Index];
+                    if ((sbyte)type != prefix)
+                    {
+                        IsValid = false;
+                        return default;
+                    }
+
+                    int result = BitConverter.ToInt32(Buffer, Index + sizeof(sbyte));
+                    Index += sizeof(sbyte) + sizeof(int);
+                    return (dynamic)result;
+                }
+
+
+                default:
+                    throw new NotImplementedException();
+            }
         }
     }
 }
