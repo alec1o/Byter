0183b1ff9b9ae3411ea1237544028e336868e336
Alecio Furanze <alecio@kezero.com>
2024-05-17 16:59:06 +0200

    add VaultPrefix on Vault

--- src/src/partials/Vault.cs
+++ src/src/partials/Vault.cs
@@ -1,15 +1,20 @@
-using System;
 using System.Collections.Generic;
 
 namespace Byter
 {
     public partial class Vault : IVault
     {
+        private static readonly IVaultPrefix Prefix;
         private readonly List<byte> _bytes;
         public bool IsValid { get; }
         public byte[] Data => _bytes.ToArray();
         public IVaultAdd Add { get; }
         public IVaultGet Get { get; }
+
+        static Vault()
+        {
+            Prefix = new VaultPrefix();
+        }
 
         public Vault() : this(null)
         {
@@ -19,9 +24,9 @@ namespace Byter
         {
             Get = new VaultGet(this);
             Add = new VaultAdd(this);
-            
+
             IsValid = false;
-            
+
             _bytes = new List<byte>();
 
             if (data != null && data.Length > 0)
