57b87a558a242dbbd8f986ec73fa6ec6b3a99c59
Alecio Furanze <alecio@kezero.com>
2024-05-19 17:12:58 +0200

    upgrade PrimitiveExtension  get bytes

--- src/extension/Primitive.cs
+++ src/extension/Primitive.cs
@@ -2,6 +2,11 @@ namespace Byter
 {
     public static class PrimitiveExtension
     {
+        public static byte[] ToPrimitive<T>(this T value)
+        {
+            return default;
+        }
+        
         public static T ToPrimitive<T>(this T value, Primitive primitive)
         {
             return value;
