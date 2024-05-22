4c105720994880377ecd98c89fe2a345368d9de3
Alecio Furanze <alecio@kezero.com>
2024-05-22 23:41:19 +0200

    Update PrimitiveExtension.cs

--- src/src/extension/PrimitiveExtension.cs
+++ src/src/extension/PrimitiveExtension.cs
@@ -18,89 +18,89 @@ namespace Byter
             object blackbox = value;
 
             // 1 byte (3)
-            if (type == typeof(bool))
+            if (value is bool)
             {
                 primitive.Add.Bool((bool)blackbox);
             }
-            else if (type == typeof(byte))
+            else if (value is byte)
             {
                 primitive.Add.Byte((byte)blackbox);
             }
-            else if (type == typeof(sbyte))
+            else if (value is sbyte)
             {
                 primitive.Add.SByte((sbyte)blackbox);
             }
             // 2 bytes (3)
-            else if (type == typeof(char))
+            else if (value is char)
             {
                 primitive.Add.Char((char)blackbox);
             }
-            else if (type == typeof(short))
+            else if (value is short)
             {
                 primitive.Add.Short((short)blackbox);
             }
-            else if (type == typeof(ushort))
+            else if (value is ushort)
             {
                 primitive.Add.UShort((ushort)blackbox);
             }
             // 4 bytes (4)
-            else if (type == typeof(int))
+            else if (value is int)
             {
                 primitive.Add.Int((int)blackbox);
             }
-            else if (type == typeof(uint))
+            else if (value is uint)
             {
                 primitive.Add.UInt((uint)blackbox);
             }
-            else if (type == typeof(float))
+            else if (value is float)
             {
                 primitive.Add.Float((float)blackbox);
             }
-            else if (type.IsEnum)
+            else if (value is Enum || type.IsEnum)
             {
                 primitive.Add.Enum(value);
             }
             // 8 bytes (3)
-            else if (type == typeof(long))
+            else if (value is long)
             {
                 primitive.Add.Long((long)blackbox);
             }
-            else if (type == typeof(ulong))
+            else if (value is ulong)
             {
                 primitive.Add.ULong((ulong)blackbox);
             }
-            else if (type == typeof(double))
+            else if (value is double)
             {
                 primitive.Add.Double((double)blackbox);
             }
-            else if (type == typeof(DateTime))
+            else if (value is DateTime)
             {
                 primitive.Add.DateTime((DateTime)blackbox);
             }
             // 16 bytes (1)
-            else if (type == typeof(decimal))
+            else if (value is decimal)
             {
                 primitive.Add.Decimal((decimal)blackbox);
             }
             // dynamic (7)
-            else if (type == typeof(string))
+            else if (value is string)
             {
                 primitive.Add.String((string)blackbox);
             }
-            else if (type == typeof(BigInteger))
+            else if (value is BigInteger)
             {
                 primitive.Add.BigInteger((BigInteger)blackbox);
             }
-            else if (type == typeof(byte[]))
+            else if (value is byte[])
             {
                 primitive.Add.Bytes((byte[])blackbox);
             }
-            else if (type.IsArray)
+            else if (type.IsArray || value is T[])
             {
                 // TODO: array
                 // primitive.Add.Array(value);
             }
-            else if (type.GetInterface(typeof(ICollection).FullName) != null)
+            else if (value is ICollection)
             {
                 // TODO: list
                 // primitive.Add.List(value);
@@ -128,7 +128,7 @@ namespace Byter
             if (type == null || primitive == null) return (default, true);
 
             object value = null;
-            
+
             // 1 byte (3)
             if (type == typeof(bool))
             {
