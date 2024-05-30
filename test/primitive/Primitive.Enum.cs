0d5c4f4227fe30e1b62e14491b34a02e143d29d6
Alecio Furanze <alecio@kezero.com>
2024-05-31 01:09:42 +0200

    Update Primitive.Enum.cs

--- test/primitive/Primitive.Enum.cs
+++ test/primitive/Primitive.Enum.cs
@@ -1,3 +1,5 @@
+using System;
+using Byter;
 using Xunit;
 
 namespace Test.Primitives;
@@ -5,7 +7,21 @@ namespace Test.Primitives;
 public partial class Primitives
 {
     [Fact]
-    public void _Enun()
+    public void _Enum()
     {
+        Primitive primitive = new();
+
+        var a = TypeCode.Empty;
+        var b = TypeCode.Object;
+        var c = TypeCode.Boolean;
+
+        primitive.Add.Enum(a);
+        primitive.Add.Enum(b);
+        primitive.Add.Enum(c);
+
+        Assert.Equal(a, primitive.Get.Enum<TypeCode>());
+        Assert.Equal(b, primitive.Get.Enum<TypeCode>());
+        Assert.Equal(b, primitive.Get.Enum<TypeCode>());
+        Assert.True(primitive.IsValid);
     }
 }
