d0b11c59bd1fcef698c8cb8001efcd2a710d2fe4
Alecio Furanze <alecio@kezero.com>
2024-05-31 15:01:23 +0200

    impl. Primitive->Reset

--- src/src/primitive/interfances/IPrimitive.cs
+++ src/src/primitive/interfances/IPrimitive.cs
@@ -1,11 +1,12 @@
 namespace Byter
 {
-    public interface IPrimitive
+    internal interface IPrimitive
     {
         int Position { get; }
         bool IsValid { get; }
         byte[] Data { get; }
         IPrimitiveAdd Add { get; }
         IPrimitiveGet Get { get; }
+        void Reset(byte[] data = null);
     }
 }
--- src/src/primitive/partials/Primitive.cs
+++ src/src/primitive/partials/Primitive.cs
@@ -5,7 +5,7 @@ namespace Byter
     public partial class Primitive : IPrimitive
     {
         private static readonly IPrimitivePrefix Prefix;
-        private readonly List<byte> _bytes;
+        private readonly List<byte> _bytes = new List<byte>();
 
         static Primitive()
         {
@@ -20,18 +20,21 @@ namespace Byter
         {
             Get = new PrimitiveGet(this);
             Add = new PrimitiveAdd(this);
-
-            IsValid = true;
-
-            _bytes = new List<byte>();
-
-            if (data != null && data.Length > 0) _bytes.AddRange(data);
+            Reset(data);
         }
 
         public int Position { get; private set; }
         public bool IsValid { get; private set; }
-        public byte[] Data => _bytes.ToArray();
+        internal byte[] Data => _bytes.ToArray();
         public IPrimitiveAdd Add { get; }
         public IPrimitiveGet Get { get; }
+
+        public void Reset(byte[] data = null)
+        {
+            Position = 0;
+            _bytes.Clear();
+            IsValid = true;
+            if (data != null && data.Length > 0) _bytes.AddRange(data);
+        }
     }
 }
